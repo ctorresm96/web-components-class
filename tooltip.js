d32c3a8d4b8e68618e5492c0b952841ecc858030
ctorresmori <carlostm320@gmail.com>
2022-02-28 18:56:33 -0500

    Javascript in the template

--- tooltip.js
+++ tooltip.js
@@ -4,18 +4,21 @@ class Tooltip extends HTMLElement {
     this._tooltipContainer;
     this._tooltipText = "Some dummy tooltip text";
     this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `
+    <slot>Some default</slot>
+    <span> (?)</span>
+ `;
   }
 
   connectedCallback() {
     if (this.hasAttribute("text")) {
       this._tooltipText = this.getAttribute("text");
     }
-
-    const tooltipIcon = document.createElement("span");
-    tooltipIcon.textContent = " (?)";
-    this.shadowRoot.appendChild(tooltipIcon);
+    const tooltipIcon = this.shadowRoot.querySelector("span");
     tooltipIcon.addEventListener("mouseenter", this._showTooltip.bind(this));
     tooltipIcon.addEventListener("mouseleave", this._hideTooltip.bind(this));
+    this.shadowRoot.appendChild(tooltipIcon);
+    this.style.position = "relative";
   }
 
   _showTooltip() {
