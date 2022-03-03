dee7cca617730703c5361e7f4491ca069eb690be
ctorresmori <carlostm320@gmail.com>
2022-03-03 14:49:33 -0500

    first component with stencil

---
+++ src/components/side-drawer/side-drawer.tsx
@@ -0,0 +1,12 @@
+import { Component, h } from '@stencil/core';
+
+@Component({ tag: 'uc-side-drawer' })
+export class SideDrawer {
+  render() {
+    return (
+      <div>
+        <h1>The side Drawer</h1>
+      </div>
+    );
+  }
+}
